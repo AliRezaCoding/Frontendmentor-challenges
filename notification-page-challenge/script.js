3b5ffaeb56c91af83b4ed280b01cf283b408371a
Ali Reza <hussinialireza9@gmail.com>
2024-05-10 16:56:02 +0430

    commit6

---
+++ notification-page-challenge/script.js
@@ -0,0 +1,24 @@
+'use strict';
+const unreadMessages = document.querySelectorAll('.noti__unread');
+const unreadCountEl = document.querySelector('.noti__count');
+const btnReadAll = document.querySelector('.btn__read--all');
+
+/////////////////////////////////////
+let unread = unreadMessages.length;
+
+const markAsRead = function () {
+    this.classList.remove('noti__unread');
+    unread > 0 ? unread-- : unread = 0;
+    unreadCountEl.textContent = unread;
+}
+
+// Here I Changed This Keyword Of Function To Message Element Using .bind() method
+unreadMessages.forEach(message => message.addEventListener('click', markAsRead.bind(message)));
+
+btnReadAll.addEventListener('click', () => {
+    unreadMessages.forEach(message => markAsRead.bind(message)());
+    unread > 0 ? unread-- : unread = 0;
+    unreadCountEl.textContent = unread;
+});
+
+
