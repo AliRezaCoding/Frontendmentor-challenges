f927040fea17308b096e792201bdc148142dcc4e
Ali Reza <hussinialireza9@gmail.com>
2024-05-18 12:47:44 +0430

    commit9

--- faq-accordion/script.js
+++ faq-accordion/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+const accordion = document.querySelector(".accordion");
+///////////////////////////////
+
+accordion.addEventListener("click", e => {
+    if (!e.target.classList.contains("question")) return;
+
+    e.target.nextElementSibling.classList.toggle("show");
+
+    // Changing Icon
+    const elIcon = e.target.querySelector(".icon-plus");
+
+    elIcon.src =
+        elIcon.getAttribute("src") === elIcon.dataset.plusUrl
+            ? elIcon.dataset.minusUrl
+            : elIcon.dataset.plusUrl;
+});
