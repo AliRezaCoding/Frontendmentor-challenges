3752538c62252db32ef8846fbb593091fbf2ce6d
Ali Reza <hussinialireza9@gmail.com>
2024-06-08 18:13:56 +0430

    intro-section-wtih-dropdown-navigation-2

--- intro-section-with-dropdown-navigation/script.js
+++ intro-section-with-dropdown-navigation/script.js
@@ -1,13 +1,32 @@
 'use strict';
 
+const navBar = document.querySelector('.nav');
 const menuBar = document.querySelector('.nav__menu');
 const btnOpenMenu = document.querySelector('.nav__icon-menu');
 const overlay = document.querySelector('.overlay');
 ////////////////////////////////////
 
+// Side bar
 btnOpenMenu.addEventListener('click', ()=>{
     // Styles set hidden by default
     menuBar.classList.toggle('nav__menu--open');
     btnOpenMenu.classList.toggle('nav__icon-menu--close');
     overlay.classList.toggle('show');
+});
+
+// Drop down menu
+navBar.addEventListener('click', e => {
+    const target = e.target;
+    if (!target.classList.contains('nav__link')) return;
+    
+    const submenuContainer = 
+    target.closest('.nav__item-submenu');
+    if(!submenuContainer) return;
+
+    // nav__icon-arrow--up
+    submenuContainer.querySelector('.nav__icon-arrow')
+    .classList.toggle('nav__icon-arrow--up');
+
+    submenuContainer.querySelector('.nav__submenu')
+    .classList.toggle('nav__submenu--open');
 });
